43f755d3d26133cf6c669eb76c6ec8b8d5e2baf6
Natiman58 <natiman58@gmail.com>
2022-10-30 17:03:41 +0300

    take this commit

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,16 @@
 $(document).ready (function() {
     let amenity_id = {}
     $(document).on('change', "input[type='checkbox']", function () {
-        if (this.checked) {
+        if ($(this).is(':checked')) {
             amenity_id[$(this).data('id')] = $(this).data('name');
         } else {
             delete amenity_id[$(this).data('id')];
         }
         let objs = Object.values(amenity_id);
         if (objs.length > 0) {
-            $('div.amenities > h4').text(Object.values(amenity_id).join(', '));
+            $('div.amenities h4').text(Object.values(amenity_id).join(', '));
         } else {
-            $('div.amenities > h4').html('&nbsp;');
+            $('div.amenities h4').html('&nbsp;');
         }
     });
 
